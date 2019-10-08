760dbab3ed0cb4010cd04232beb87dfbc81dc606
Kyle Leonard <kdleonard@icloud.com>
2019-10-08 11:26:57 -0500

    Created the setValue function

--- src/components/Hooks/UseLocalStorage.js
+++ src/components/Hooks/UseLocalStorage.js
@@ -6,7 +6,12 @@ const [storedValue, setStoredValue] = useState(() => {
     const item = window.localStorage.getItem(key);
 
     return item ? JSON.parse(item) : initialValue;
-});
+}); 
+
+const setValue = value => {
+    setStoredValue = value;
+    window.localStorage.setItem(key, JSON.stringify(value));
+ }
 
 return [storedValue]
 }
