f0bc44bf14aba230fd682daa2566422017b42640
Kyle Leonard <kdleonard@icloud.com>
2019-10-08 10:54:17 -0500

    commented out import for now

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-import {useLocalStorage} from "./components/Hooks/UseLocalStorage"
+// import {useLocalStorage} from "./components/Hooks/UseLocalStorage"
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
